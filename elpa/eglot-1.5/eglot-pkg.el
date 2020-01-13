844d8064a26f3bc96062d4f6f870ec1e90ac7b4f
maroon297 <nirvana297@gmail.com>
2020-01-13 22:26:37 +0900

    全体的に見直し Linuxに最適化

---
+++ elpa/eglot-1.5/eglot-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "eglot" "1.5" "Client for Language Server Protocol (LSP) servers" '((emacs "26.1") (jsonrpc "1.0.7") (flymake "1.0.5")) :commit "33a4f869972f0958c15c33b47035672b265a8b55" :keywords '("convenience" "languages") :authors '(("João Távora" . "joaotavora@gmail.com")) :maintainer '("João Távora" . "joaotavora@gmail.com") :url "https://github.com/joaotavora/eglot")
