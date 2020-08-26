d02fe68f58c39517e9e57c139d4b82333d7f6ff9
maroon297 <nirvana297@gmail.com>
2020-08-26 23:02:41 +0900

    change

--- elpa/mozc-im-20160412.22/mozc-im-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "mozc-im" "20160412.22" "Mozc with input-method-function interface." '((mozc "0")) :commit "df614a1076c28a11551fb3e822868bae47e855a5" :keywords '("i18n" "extentions") :authors '(("Daisuke Kobayashi" . "d5884jp@gmail.com")) :maintainer '("Daisuke Kobayashi" . "d5884jp@gmail.com"))
--- init.el
+++ init.el
@@ -12,8 +12,6 @@
 ;; プロセスが出力する文字コードを判定して、process-coding-system の DECODING の設定値を決定する
 (setq default-process-coding-system '(undecided-dos . utf-8-unix))
 
-(package-initialize)
-
 (add-to-list 'load-path "~/.emacs.d/site-lisp")
 
 ;;------------------------;;
@@ -23,7 +21,6 @@
 (cond(window-system
   (setq x-select-enable-clipboard t)
  ))
-
 
 ;; *.~ とかのバックアップファイルを作らない
 (setq make-backup-files nil)
@@ -68,7 +65,6 @@
 
 ;;------------------------;;
 
-
 ;; Window表示時のみ、テーマと透過度を設定する
 (if window-system
     (progn
@@ -85,7 +81,6 @@
 ;;(setq tab-width 4)
 ;; タブにスペースを使用する
 (setq-default tab-width 4 indent-tabs-mode nil)
-
 
 ;; default scroll bar消去
 (scroll-bar-mode -1)
@@ -136,7 +131,7 @@
 ;;(global-hl-line-mode t)
 
 ;; スクリーンの最大化
-;;(set-frame-parameter nil 'fullscreen 'maximized)
+(set-frame-parameter nil 'fullscreen 'maximized)
 
 ;; 起動時のフレーム設定
 (setq initial-frame-alist
@@ -153,127 +148,108 @@
 ;;閉じ括弧を自動的に追加
 (electric-pair-mode 1)
 
-;; El-Get設定
-(when load-file-name
-  (setq user-emacs-directory (file-name-directory load-file-name)))
+;;------------------------;;
+;;パッケージ設定
+;;------------------------;;
 
-(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
+(require 'package)
 
-;;async設定(Helmにいる？)
-;(el-get-bundle async)
+(when (not (assoc "melpa" package-archives))
+  (setq package-archives (append '(("stable" . "https://stable.melpa.org/packages/")) package-archives))
+  (setq package-archives (append '(("melpa" . "https://melpa.org/packages/")) package-archives))
+  (setq package-archives (append '(("gnu" . "https://elpa.gnu.org/packages/")) package-archives)))
 
-;;Helm設定
-;;(el-get-bundle helm)
-;(require 'helm-config)
-;(helm-mode 1)
+(package-initialize)
 
-;;helm-miniキーマップ
-;(global-set-key (kbd "C-c h") 'helm-mini)
+;; Install use-package if not already installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-;;M-xをhelm-M-xに
-;(global-set-key (kbd "M-x") 'helm-M-x)
+(require 'use-package)
 
-;;キルリングを表示する
-;(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+;; Enable defer and ensure by default for use-package
+;; Keep auto-save/backup files separate from source code:  https://github.com/scalameta/metals/issues/1027
+(setq use-package-always-defer t
+      use-package-always-ensure t
+      backup-directory-alist `((".*" . ,temporary-file-directory))
+      auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
 
-;;helm-find-files
-;(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(use-package counsel
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  (setq ivy-height 30) ;; minibufferのサイズを拡大！（重要）
+  (setq ivy-extra-directories nil)
+  (setq ivy-re-builders-alist
+        '((t . ivy--regex-plus)))
+  (setq counsel-find-file-ignore-regexp (regexp-opt '("./" "../")))
+  :bind (("M-x" . 'counsel-M-x)
+         ("C-x C-f" . 'counsel-find-file)
+         )
+  )
+;;avy
+(use-package avy)
 
+;;magit
+(use-package magit
+  :bind (("C-x g" . 'magit-status))  
+  )
 
+(use-package company
+  :ensure t
+  :config
+  (setq company-auto-expand t) ;; 1個目を自動的に補完
+  (setq company-transformers '(company-sort-by-backend-importance)) ;; ソート順
+  (setq company-idle-delay 0) ; 遅延なしにすぐ表示
+  (setq company-minimum-prefix-length 2) ; デフォルトは4
+  (setq company-selection-wrap-around t) ; 候補の最後の次は先頭に戻る
+  (setq completion-ignore-case t)
+  (setq company-dabbrev-downcase nil)
+  (set-face-attribute 'company-tooltip nil
+                      :foreground "black" :background "lightgrey")
+  (set-face-attribute 'company-tooltip-common nil
+                      :foreground "black" :background "lightgrey")
+  (set-face-attribute 'company-tooltip-common-selection nil
+                      :foreground "white" :background "steelblue")
+  (set-face-attribute 'company-tooltip-selection nil
+                      :foreground "black" :background "steelblue")
+  (set-face-attribute 'company-preview-common nil
+                      :background nil :foreground "lightgrey" :underline t)
+  (set-face-attribute 'company-scrollbar-fg nil
+                      :background "orange")
+  (set-face-attribute 'company-scrollbar-bg nil
+                      :background "gray40")
+  :bind
+  (("C-M-i" . company-complete))
+  (:map company-active-map
+        ("C-n" . company-select-next)
+        ("C-p" . company-select-previous)
+        ("C-s" . company-filter-candidates)
+        ("C-i" . company-complete-selection)
+        ([tab] . company-complete-selection))
+  (:map company-search-map
+        ("C-n" . company-select-next)
+        ("C-p" . company-select-previous))
+  :init
+  (global-company-mode t)
+  )
+
+(use-package flymake)
+(use-package rustic)
+(setq rustic-lsp-server 'rust-analyzer)
+(setq lsp-rust-analyzer-server-command '("~/.cargo/bin/rust-analyzer"))
+;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company graphql magit-popup async))))
+ '(package-selected-packages (quote (company lsp-mode magit counsel use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-;; neotree をインストールする
-(el-get-bundle neotree)
-;; F8でnetreee-windowが開くようにする
-(global-set-key [f8] 'neotree-toggle)
-
-;;company
-(el-get-bundle company-mode/company-mode)
-(with-eval-after-load 'company
-      (setq company-auto-expand t) ;; 1個目を自動的に補完
-      (setq company-transformers '(company-sort-by-backend-importance)) ;; ソート順
-      (setq company-idle-delay 0) ; 遅延なしにすぐ表示
-      (setq company-minimum-prefix-length 2) ; デフォルトは4
-      (setq company-selection-wrap-around t) ; 候補の最後の次は先頭に戻る
-      (setq completion-ignore-case t)
-      (setq company-dabbrev-downcase nil)
-      (global-set-key (kbd "C-M-i") 'company-complete)
-      ;; C-n, C-pで補完候補を次/前の候補を選択
-      (define-key company-active-map (kbd "C-n") 'company-select-next)
-      (define-key company-active-map (kbd "C-p") 'company-select-previous)
-      (define-key company-active-map [tab] 'company-complete-selection) ;; TABで候補を設定
-      (define-key company-active-map (kbd "C-h") nil) ;; C-hはバックスペース割当のため無効化
-      (define-key company-active-map (kbd "C-S-h") 'company-show-doc-buffer) ;; ドキュメント表示はC-Shift-h
-
-      ;; 未選択項目
-      (set-face-attribute 'company-tooltip nil
-                  :foreground "#36c6b0" :background "#244f36")
-      ;; 未選択項目&一致文字
-      (set-face-attribute 'company-tooltip-common nil
-                    :foreground "white" :background "#244f36")
-      ;; 選択項目
-      (set-face-attribute 'company-tooltip-selection nil
-                  :foreground "#a1ffcd" :background "#007771")
-      ;; 選択項目&一致文字
-      (set-face-attribute 'company-tooltip-common-selection nil
-                    :foreground "white" :background "#007771")
-      ;; スクロールバー
-      (set-face-attribute 'company-scrollbar-fg nil
-                  :background "#4cd0c1")
-      ;; スクロールバー背景
-      (set-face-attribute 'company-scrollbar-bg nil
-                  :background "#002b37")
-      )
-;;fly-check
-(el-get-bundle flycheck)
-(add-hook 'after-init-hook #'global-flycheck-mode)
-
-;;PlantUML設定
-(el-get-bundle plantuml-mode)
-;; plantuml.jar の path を設定
-(setq org-plantuml-jar-path "~/.emacs.d/plantuml.jar")
-(add-to-list 'auto-mode-alist '("\\.puml\\'" . plantuml-mode))
-(add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode))
-(add-to-list 'auto-mode-alist '("\\.pu\\'" . plantuml-mode))
-(setq plantuml-options "-charset UTF-8")
-			 
-
-(el-get-bundle markdown-mode)
-(autoload 'markdown-mode "markdown-mode"
-   "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
-
-;; markdownのコマンドのパス追加
-(setq markdown-command "jq --slurp --raw-input '{\"text\": \"\\(.)\", \"mode\": \"gfm\"}' | curl -sS --data @- https://api.github.com/markdown")
-
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(fset 'package-desc-vers 'package--ac-desc-version)
-(package-initialise)
-
-;;; mozc
-(require 'mozc)                                 ; mozcの読み込み
-(set-language-environment "Japanese")           ; 言語環境を"japanese"に
-(setq default-input-method "japanese-mozc")     ; IMEをjapanes-mozcに
-(prefer-coding-system 'utf-8)                   ; デフォルトの文字コードをUTF-8に
-
-;;; init.el ends here
